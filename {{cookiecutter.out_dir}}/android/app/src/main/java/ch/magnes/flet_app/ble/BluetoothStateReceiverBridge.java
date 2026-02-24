1ea11324d3cb7eb31f588fc52042d71e037faa67
conradmagnes <conradm@magnes.ch>
2026-02-24 23:04:27 +0100

    +bluetooth state receiver shim

---
+++ {{cookiecutter.out_dir}}/android/app/src/main/java/ch/magnes/flet_app/ble/BluetoothStateReceiverBridge.java
@@ -0,0 +1,41 @@
+package ch.magnes.flet_app.ble;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * BluetoothStateReceiverBridge forwards ACTION_STATE_CHANGED events
+ * to a listener interface that Pyjnius can implement.
+ */
+public class BluetoothStateReceiverBridge extends BroadcastReceiver {
+
+    /** Listener interface implemented by PythonJavaClass. */
+    public interface BluetoothStateListener {
+        void onBluetoothStateChanged(int state);
+    }
+
+    private BluetoothStateListener listener;
+
+    public void setListener(BluetoothStateListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (listener == null || intent == null) {
+            return;
+        }
+
+        String action = intent.getAction();
+        if (!BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+            return;
+        }
+
+        int state = intent.getIntExtra(
+                BluetoothAdapter.EXTRA_STATE,
+                BluetoothAdapter.ERROR);
+        listener.onBluetoothStateChanged(state);
+    }
+}
